0db68c1111e3ab5685bb144ec3233dfcb8e8c81f
Macy Tiradoegas <mtiradoegas@w-mtiradoegasmbp.lan>
2019-04-20 13:17:17 -0500

    Section 3

---
+++ section3/app.js
@@ -0,0 +1,15 @@
+const notes = require('./notes.js');
+
+let firstNote = notes("Yoooo this is a note");
+
+console.log(firstNote);
+
+
+
+// const add = require('./utils.js');
+// const name = require('./yo.js');
+
+// const sum = add(2, 9);
+
+// console.log(sum);
+// console.log(name);
---
+++ section3/notes.js
@@ -0,0 +1,5 @@
+const getNotes = (str) => {
+	return str
+}
+
+module.exports = getNotes;
