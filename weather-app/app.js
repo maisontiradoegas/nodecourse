18ff805207b3c81c39f3f88dd2abbd6716531e60
maisontiradoegas <maisontiradoegas@yahoo.com>
2019-05-08 06:22:47 -0500

    Abstraction Callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -15,14 +15,22 @@ const request = require('request');
 // Geo Coding - the process of taking an address like Philly and converting that into a latitude / longitude coordinate pair.
 // Address -> Lat/Long -> Weather
 
-const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/yo.json?access_token=pk.eyJ1IjoibWFpc29udGlyYWRvZWdhcyIsImEiOiJjanZiYW1xNXMwZDQxNDFwOHZtMzRkMTc2In0.JLnYmUYq9Pwp6nxZXbiy4A&limit=1';
+// const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/yo.json?access_token=pk.eyJ1IjoibWFpc29udGlyYWRvZWdhcyIsImEiOiJjanZiYW1xNXMwZDQxNDFwOHZtMzRkMTc2In0.JLnYmUYq9Pwp6nxZXbiy4A&limit=1';
 
-request({ url: url2, json: true }, (error, response) => {
-    if(error){
-        console.log('Error Occured');
-    } else if(response.body.features.length === 0){
-        console.log('No Matching Results');
-    }  else {
-        console.log(response.body.features[0].center);
-    }
+// request({ url: url2, json: true }, (error, response) => {
+//     if(error){
+//         console.log('Error Occured');
+//     } else if(response.body.features.length === 0){
+//         console.log('No Matching Results');
+//     }  else {
+//         console.log(response.body.features[0].center);
+//     }
+// })
+
+const geoCode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWFpc29udGlyYWRvZWdhcyIsImEiOiJjanZiYW1xNXMwZDQxNDFwOHZtMzRkMTc2In0.JLnYmUYq9Pwp6nxZXbiy4A&limit=1'
+}
+
+geoCode('Texas', (err, data) => {
+
 })
