239e887f5a422fbd253b1ee937e65a22a3e2d456
Maison Tiradoegas <maisontiradoegas@yahoo.com>
2019-05-13 09:27:53 -0500

    core https requests

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -1,3 +1,22 @@
 const https = require('https');
+const url = 'https://api.darksky.net/forecast/6147f41f7be1da4d64e83983f04c53a6/40,-75';
 
-const url = 'api.darksky.net/forecast/6147f41f7be1da4d64e83983f04c53a6/40,-75';
+const request = https.request(url, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+        data = data + chunk.toString();
+    })
+
+    response.on('end', () => {
+        const body = JSON.parse(data);
+        console.log(body);
+    })
+
+})
+
+request.on('error', (error) => {
+    console.log('An Error', error)
+})
+
+request.end()
