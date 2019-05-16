e97727feb0e3c6d982868acff3c6d0d15af4d9d2
maison-at-471317562093 <maisontiradoegas@yahoo.com>
2019-05-16 07:56:53 -0500

    Setup app for Heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,8 @@ const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-const app = express(); 
+const app = express();
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -66,7 +67,6 @@ app.get('/weather', (req, res) => {
             });
         })
     })
-   
 })
 
 app.get('/help/*', (req, res) => {
@@ -98,6 +98,6 @@ app.get('*', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
